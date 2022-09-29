405e8b7e54416a7c90097ecbac155065235f489e
Sedenilke <sedenilke@gmail.com>
2022-09-28 17:07:01 -0700

    third commit

--- src/FinalProject/FinalClass.java
+++ src/FinalProject/FinalClass.java
@@ -5,6 +5,7 @@ public class FinalClass {
 	public static void main(String[] args) {
 		System.out.println("this is the code for the mercede");
 		System.out.println("Hello there");
+		System.out.println("This will be my third change");
 
 	}
 
